bebc796dfcea0626be86592594535dea316cda55
Chris Baum <Christopher.Baum@valpo.edu>
2021-09-09 21:21:47 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validate :clickbait_title?
+
+  category_inclusion_keywords = ["Fiction", "Non-Fiction"]
+  validates :category, inclusion: {in: category_inclusion_keywords}
+
+  CLICKBAIT= ["Won't Believe", "Secret", "Guess"]
+  i = 0
+
+  # Top 1 to Top 100 add to list
+  while i <= 100 do
+    CLICKBAIT << "Top " + i.to_s
+    i = i + 1
+  end
+
+
+  def clickbait_title?
+    if title == nil || CLICKBAIT.none? {|keyword| title.include?(keyword)}
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
